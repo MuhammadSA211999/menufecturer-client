4b46ccdbdda4bd41ca252ae3d584bb45b8560677
Muhammad SA <MuhammadSA211999>
2022-05-26 14:57:30 +0600

    data load done

--- src/page/DashBoard/AllOrders.js
+++ src/page/DashBoard/AllOrders.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const AllOrders = ({ order, index, refetch }) => {
-    const { email, price, productName } = order;
-    return (
-        <tr>
-        <th>{index+1}</th>
-        <td>{productName}</td>
-        <td>{price}</td>
-        <td>{email}</td>
-        <td><button className="btn btn-sm">Delete</button></td>
-      </tr>
-    );
+  // destructure
+  const { email, price, productName } = order;
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <td>{productName}</td>
+      <td>{price}</td>
+      <td>{email}</td>
+      <td><button className="btn btn-sm">Delete</button></td>
+    </tr>
+  );
 };
 
 export default AllOrders;
